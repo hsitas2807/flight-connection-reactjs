d2db13871a112c1588ca5a1099762ddb3073afc1
Parul Bajpai <parul.hexooid@gmail.com>
2021-03-06 16:13:16 +0530

    correct time converter logic

--- src/utils/TimeConverter.js
+++ src/utils/TimeConverter.js
@@ -1,14 +1,14 @@
-import React from "react";
-
 
 export const TimeConverter = ({ unixTime }) => {
-  var createDate = new Date(unixTime);
+  var createDate = new Date(unixTime * 1000);
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   var year = createDate.getFullYear();
   var month = months[createDate.getMonth()];
   var date = createDate.getDate();
-
-  var time = date + ' ' + month + ' ' + year;
+  var hour = createDate.getHours();
+  var min = createDate.getMinutes();
+  var sec = createDate.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
   return time;
 
 };
